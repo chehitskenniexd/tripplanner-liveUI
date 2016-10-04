3399f08c3223e14d14f7f583d4a3cab94afbc1dd
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-10-04 18:22:11 -0400

    added itinerary

--- public/js/control-panel.js
+++ public/js/control-panel.js
@@ -2,12 +2,47 @@
 
 // ensures that the document is ready
 $(document).ready(function () {
+	var hotelChoices = $('#hotel-choices');
+	var restaurantChoices = $('#restaurant-choices');
+	var activityChoices = $('#activity-choices');
+
 
     hotels.forEach(hotel =>
-        $('#hotel-choices').append(`<option>${hotel.name}</option>`))
+        hotelChoices.append(`<option>${hotel.name}</option>`))
     restaurants.forEach(restaurant =>
-        $('#restaurant-choices').append(`<option>${restaurant.name}</option>`)) 
+        restaurantChoices.append(`<option>${restaurant.name}</option>`)) 
     activities.forEach(activity =>
-        $('#activity-choices').append(`<option>${activity.name}</option>`))   
+        activityChoices.append(`<option>${activity.name}</option>`))  
+
+
+    $('.add-hotel').click(function(){
+    	var hotelName = hotelChoices.val();
+
+    	$('.hotel-list').append(`<div class="itinerary-item">\n
+                <span class="title">${hotelName}</span>\n
+                <button class="btn btn-xs btn-danger remove btn-circle">x</button>\n
+              </div>\n`)
+    })
+
+      $('.add-restaurant').click(function(){
+    	var restaurantName = restaurantChoices.val();
+
+    	$('.restaurant-list').append(`<div class="itinerary-item">\n
+                <span class="title">${restaurantName}</span>\n
+                <button class="btn btn-xs btn-danger remove btn-circle">x</button>\n
+              </div>\n`)
+    })
+
+
+  $('.add-activity').click(function(){
+    	var activityName = activityChoices.val();
+
+    	$('.activity-list').append(`<div class="itinerary-item">\n
+                <span class="title">${activityName}</span>\n
+                <button class="btn btn-xs btn-danger remove btn-circle">x</button>\n
+              </div>\n`)
+    })
+
+
 
 }) // end document ready
